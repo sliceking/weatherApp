787110b8b4792a96e192842d79009cd1926157ba
stan wielga <svwielga4@gmail.com>
2018-01-06 19:25:25 -0800

    updated search bar and weather api requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-import apiKey from '../private';
+import API_KEY from '../private';
+import axios from 'axios';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+
+    }
+}
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component{
     constructor(props){
         super(props);
         this.state = { term:'' };
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
@@ -13,6 +17,8 @@ class SearchBar extends Component{
 
     onFormSubmit(event){
         event.preventDefault();
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' }); //clear form
     }
 
     render(){
@@ -31,4 +37,8 @@ class SearchBar extends Component{
     }
 }
 
-module.exports = SearchBar;
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+module.exports = connect(null, mapDispatchToProps)(SearchBar);
